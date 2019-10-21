2aa67a3a2eca5d65bfdda61472a09a80ba971aab
Shubham Singh <shubham.singh12@infosys.com>
2019-10-21 19:39:40 +0530

    Added stylesheet and Javascript

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('Connected')
+
+boxes = document.querySelectorAll(".boxes")
+
+console.log(boxes)
+
+
+var colors = [];
+
+
+function colorizeBoxes() {
+    
+}
+
+
+
+function generateRandomColors() {
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 256)
+    var color = `rgb(${r}, ${g}, ${b})`
+    boxes[0].style.background = color
+
+    console.log(color)
+}
+
+
+generateRandomColors();
