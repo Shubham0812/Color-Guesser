839fd330979a5dd4998e554224ddf1dc2b7c4198
Shubham Singh <shubham.singh12@infosys.com>
2019-10-22 22:06:14 +0530

    Functionality added

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-console.log('Connected')
-
 boxes = document.querySelectorAll(".boxes")
 guess = document.querySelector(".colorGuess")
 winnerScore = document.querySelector(".processing-overlay")
 scoreMessage = document.querySelector(".processing")
 
-gameSize = 6
-// console.log(boxes)
+gameSizes = {
+    "easy": {
+        "tries": 1,
+        "tiles": 3
+    },
+    "hard": {
+        "tries": 3,
+        "tiles": 6
+    }
+}
+
+mode = "hard"
+
+life = 0
+
 
 correctColor = ""
 var colors = [];
@@ -16,38 +27,51 @@ function colorizeBoxes() {
     for (let i = 0; i < boxes.length; i++) {
         this.boxes[i].style.background = colors.pop();
         this.boxes[i].addEventListener("click", function () {
-            console.log("inside event", this.style.background)
+
+            if (life === 0) {
+                console.log("Game over")
+            }
+
             if (this.style.background === correctColor) {
                 winnerScore.style.background = correctColor
                 var color = correctColor.replace("rgb(", "").replace(")", "").split(',')
-                scoreMessage.style.background = `rgba(${color[0] + 10}, ${color[1] + 10}, ${color[2] + 10}, 0.4)`
-                console.log("checking", color)
-                winnerScore.style.display = "flex"
-                winnerScore.classList.add("fade-up")
+                scoreMessage.style.backgroundColor = `rgba(${color[0] + 10}, ${color[1] + 10}, ${color[2] + 10}, 0.5)`
+                winnerScore.classList.add("show-message")
                 setTimeout(function () {
-                    console.log("Time to remove")
-                    winnerScore.classList.remove("fade-up")
-                    winnerScore.style.display = "none"
-                }, 1800)
+                    winnerScore.classList.remove("show-message")
+                }, 950)
 
-            } else {
+            }
+            else {
                 this.classList.add("fade-down");
+                var box = this
+                setTimeout(function () {
+
+                    box.parentNode.removeChild(box)
+                }, 600)
+                life -= 1
+                if (life === 0) {
+                    console.log("Game over")
+
+                }
             }
         })
     }
 }
 
 
-function colorPicked(box) {
-    console.log(box.srcElement.style.background)
-    if (box.srcElement.style.background === correctColor) {
-    } else {
+function contrastCheck() {
+    var color = correctColor.replace("rgb(", "").replace(")", "").split(',')
+    if ((color[0] * 0.299 + color[1] * 0.587 + color[2] * 0.114) < 0.1) {
+        return true
+    }
+    else {
+        return false
     }
 }
 
-
 function generateRandomColors() {
-    for (let i = 0; i < gameSize; i++) {
+    for (let i = 0; i < gameSizes[mode].tiles; i++) {
         let r = Math.floor(Math.random() * 256)
         let g = Math.floor(Math.random() * 256)
         let b = Math.floor(Math.random() * 256)
@@ -58,9 +82,15 @@ function generateRandomColors() {
         return Math.random() - 0.5;
     })[0]
     guess.innerHTML = correctColor
-
-    console.log("Color to guess", correctColor)
+    life = gameSizes[mode].tries
 }
 
 generateRandomColors();
 colorizeBoxes();
+
+
+function reset() {
+    colors = [];
+    generateRandomColors();
+    colorizeBoxes();
+}
